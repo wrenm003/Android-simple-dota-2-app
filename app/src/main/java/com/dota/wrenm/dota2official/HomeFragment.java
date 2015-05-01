1133096aa36c8e4a54102d073e559657dab77750
wrenm003 <wrenm003@gmail.com>
2015-05-01 15:04:10 +0300

    BasicProject
    Starting to build Dota2 Official Android Application

---
+++ app/src/main/java/com/dota/wrenm/dota2official/HomeFragment.java
@@ -0,0 +1,59 @@
+package com.dota.wrenm.dota2official;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        WebView webView = (WebView)rootView.findViewById(R.id.webView);
+
+        webView.setInitialScale(1);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        webView.setScrollbarFadingEnabled(false);
+
+        String strUrl="http://blog.dota2.com/" ;
+
+        webView.loadUrl(strUrl);
+        webView.setWebViewClient(new WebViewClient());
+
+
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
---
+++ app/src/main/java/com/dota/wrenm/dota2official/MessagesFragment.java
@@ -0,0 +1,47 @@
+package com.dota.wrenm.dota2official;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MessagesFragment extends Fragment {
+
+
+    public MessagesFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
+
+        WebView webView = (WebView)rootView.findViewById(R.id.webView);
+
+        webView.setInitialScale(1);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        webView.setScrollbarFadingEnabled(false);
+
+        String strUrl="http://blog.dota2.com/" ;
+
+        webView.loadUrl(strUrl);
+        webView.setWebViewClient(new WebViewClient());
+
+        return rootView;
+
+    }
+}
